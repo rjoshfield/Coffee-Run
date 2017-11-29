479ab1b486c350a6e0edfd9eb713e3b39999cf20
slugula <32847885+slugula@users.noreply.github.com>
2017-11-29 09:15:22 -0500

    Adding level generation

---
+++ Coffee Run/Assets/_Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	void Start()
+	{
+		
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y++;
+			this.transform.position = position * speed;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y--;
+			this.transform.position = position * speed;
+		}
+	}
+}
