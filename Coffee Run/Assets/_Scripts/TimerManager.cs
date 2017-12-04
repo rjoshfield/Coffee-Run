30615092e5024f6455a34bebbc70400e8483921b
slugula <32847885+slugula@users.noreply.github.com>
2017-12-03 22:02:27 -0500

    d

---
+++ Coffee Run/Assets/_Scripts/TimerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TimerManager : MonoBehaviour {
+	public Image timerBar;
+	public float timeAmt = 100;
+	public float time;
+	public Text timeText;
+
+	void Start () {
+		timerBar = timerBar.GetComponent<Image> ();
+		timeText = timeText.GetComponent<Text> ();
+		time = timeAmt;
+	}
+
+	void Update () {
+		if (time > 0) {
+			time -= Time.deltaTime;
+			timerBar.fillAmount = time / timeAmt;
+			timeText.text = time.ToString ("N0");
+		}
+	}
+}
